73e64acc53cf079ba2aad030dca2d07f24758a9e
DEAN <>
2018-08-29 14:23:24 +0900

    #pro_header 삭제

--- theme/propaganda/head.php
+++ theme/propaganda/head.php
@@ -20,36 +20,34 @@ include_once(G5_LIB_PATH.'/popular.lib.php');
 ?>
 
 <!-- dean_상단 시작 { -->
-<div id="pro_header">
-    <header class="pro_hd">
-        <div class="ca clearfix">
-            <h1 class="logo">
-                <a href="<?php echo G5_URL ?>" class="ir">
-                    PROPAGANDA
-                </a>
-            </h1>
-            <div id="mn_btn">
-                <a href="#" class="mn_opn">
-                    <img src="<?php echo G5_THEME_IMG_URL; ?>/pro_mn_opn.png" alt="메뉴 열기">
-                </a>
-                <a href="#" class="mn_clse">
-                    <img src="<?php echo G5_THEME_IMG_URL; ?>/pro_mn_clse.png" alt="메뉴 닫기">
-                </a>
-            </div>
-            <ul class="gnb">
-                <li class="menu mn_work">
-                    <a href="<?php echo G5_BBS_URL; ?>/board.php?bo_table=WORK">WORK</a>
-                </li>
-                <li class="menu mn_board">
-                    <a href="<?php echo G5_BBS_URL; ?>/board.php?bo_table=BOARD">BOARD</a>
-                </li>
-                <li class="menu mn_store">
-                    <a href="<?php echo G5_SHOP_URL; ?>">STORE</a>
-                </li>
-            </ul>
+<header class="pro_hd">
+    <div class="ca clearfix">
+        <h1 class="logo">
+            <a href="<?php echo G5_URL ?>" class="ir">
+                PROPAGANDA
+            </a>
+        </h1>
+        <div id="mn_btn">
+            <a href="#" class="mn_opn">
+                <img src="<?php echo G5_THEME_IMG_URL; ?>/pro_mn_opn.png" alt="메뉴 열기">
+            </a>
+            <a href="#" class="mn_clse">
+                <img src="<?php echo G5_THEME_IMG_URL; ?>/pro_mn_clse.png" alt="메뉴 닫기">
+            </a>
         </div>
-    </header>
-</div>
+        <ul class="gnb clearfix">
+            <li class="menu mn_work">
+                <a href="<?php echo G5_BBS_URL; ?>/board.php?bo_table=WORK">WORK</a>
+            </li>
+            <li class="menu mn_board">
+                <a href="<?php echo G5_BBS_URL; ?>/board.php?bo_table=BOARD">BOARD</a>
+            </li>
+            <li class="menu mn_store">
+                <a href="<?php echo G5_SHOP_URL; ?>">STORE</a>
+            </li>
+        </ul>
+    </div>
+</header>
 <!-- } 상단 끝 -->
 
 <!-- 콘텐츠 시작 { -->
