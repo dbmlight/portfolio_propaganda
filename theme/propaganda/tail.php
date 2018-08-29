6f6dcbcfb8007a9c09ec05c00914b8d0bed48b29
DEAN <>
2018-08-29 16:16:32 +0900

    상단 버튼 코딩 위치 변경

--- theme/propaganda/tail.php
+++ theme/propaganda/tail.php
@@ -20,14 +20,14 @@ if(G5_COMMUNITY_USE === false) {
             Copyright (c) 2009 PROPAGANDA. All rights reserved.
         </div>
         <div class="fb-like" data-href="https://www.facebook.com/PropagandaGraphics" data-layout="button" data-action="like" data-show-faces="false" data-share="true"></div>
-        <a href="#" class="ir pro_btn_top">
-            상단으로
-        </a>
         <p class="pro_validator">
             <a href="http://validator.kldp.org/check?uri=referer" onclick="this.href=this.href.replace(/referer$/,encodeURIComponent(document.URL))">
                 <img src="//validator.kldp.org/w3cimgs/validate/html5-blue.png" alt="Valid HTML 5" height="15" width="80">
             </a>
         </p>
+        <a href="#" class="ir pro_btn_top">
+            상단으로
+        </a>
     </div>
 </footer>
 <!-- } 하단 끝 -->
