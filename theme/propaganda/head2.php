5c04345642ddf5ea424734eaa862ce3859ef79df
DEAN <>
2018-08-30 16:30:43 +0900

    skin 용 head2, tail2 추가

---
+++ theme/propaganda/head2.php
@@ -0,0 +1,55 @@
+<?php
+if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
+
+if (G5_IS_MOBILE) {
+    include_once(G5_THEME_MOBILE_PATH.'/head.php');
+    return;
+}
+
+if(G5_COMMUNITY_USE === false) {
+    include_once(G5_THEME_SHOP_PATH.'/shop.head.php');
+    return;
+}
+include_once(G5_THEME_PATH.'/head.sub.php');
+include_once(G5_LIB_PATH.'/latest.lib.php');
+include_once(G5_LIB_PATH.'/outlogin.lib.php');
+include_once(G5_LIB_PATH.'/poll.lib.php');
+include_once(G5_LIB_PATH.'/visit.lib.php');
+include_once(G5_LIB_PATH.'/connect.lib.php');
+include_once(G5_LIB_PATH.'/popular.lib.php');
+?>
+
+<!-- dean_상단 시작 { -->
+<header class="pro_hd">
+    <div class="ca clearfix">
+        <h1 class="logo">
+            <a href="<?php echo G5_URL ?>" class="ir">
+                PROPAGANDA
+            </a>
+        </h1>
+        <div id="mn_btn">
+            <a href="#" class="mn_opn">
+                <img src="<?php echo G5_THEME_IMG_URL; ?>/pro_mn_opn.png" alt="메뉴 열기">
+            </a>
+            <a href="#" class="mn_clse">
+                <img src="<?php echo G5_THEME_IMG_URL; ?>/pro_mn_clse.png" alt="메뉴 닫기">
+            </a>
+        </div>
+        <ul class="gnb clearfix">
+            <li class="menu mn_work">
+                <a href="<?php echo G5_BBS_URL; ?>/board.php?bo_table=WORK">WORK</a>
+            </li>
+            <li class="menu mn_board">
+                <a href="<?php echo G5_BBS_URL; ?>/board.php?bo_table=BOARD">BOARD</a>
+            </li>
+            <li class="menu mn_store">
+                <a href="<?php echo G5_SHOP_URL; ?>">STORE</a>
+            </li>
+        </ul>
+    </div>
+</header>
+<!-- } 상단 끝 -->
+
+<!-- 콘텐츠 시작 { -->
+<div class="pro_container clearfix">
+<div class="ca">
---
+++ theme/propaganda/tail2.php
@@ -0,0 +1,44 @@
+<?php
+if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
+
+if (G5_IS_MOBILE) {
+    include_once(G5_THEME_MOBILE_PATH.'/tail.php');
+    return;
+}
+
+if(G5_COMMUNITY_USE === false) {
+    include_once(G5_THEME_SHOP_PATH.'/shop.tail.php');
+    return;
+}
+?>
+</div><!-- .pro_container .ca end -->
+</div><!-- .pro_container end -->
+<!-- } 콘텐츠 끝 -->
+<!-- 하단 시작 { -->
+<footer class="pro_ft">
+    <div class="ca clearfix">
+        <div class="copyright">
+            Copyright (c) 2009 PROPAGANDA. All rights reserved.
+        </div>
+        <div class="fb-like" data-href="https://www.facebook.com/PropagandaGraphics" data-layout="button" data-action="like" data-show-faces="false" data-share="true"></div>
+        <p class="pro_validator">
+            <a href="http://validator.kldp.org/check?uri=referer" onclick="this.href=this.href.replace(/referer$/,encodeURIComponent(document.URL))">
+                <img src="//validator.kldp.org/w3cimgs/validate/html5-blue.png" alt="Valid HTML 5" height="15" width="80">
+            </a>
+        </p>
+        <a href="#" class="ir pro_btn_top">
+            상단으로
+        </a>
+    </div>
+</footer>
+<!-- } 하단 끝 -->
+<script>
+$(function() {
+    // 폰트 리사이즈 쿠키있으면 실행
+    font_resize("container", get_cookie("ck_font_resize_rmv_class"), get_cookie("ck_font_resize_add_class"));
+});
+</script>
+
+<?php
+include_once(G5_THEME_PATH."/tail.sub.php");
+?>
