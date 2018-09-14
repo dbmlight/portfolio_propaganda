19a553e2641ff6f5e8517ce5b5ffe8d3b0db9446
dbmlight <dbmlight@gmail.com>
2018-09-15 00:37:10 +0900

    최신글 스킨 추가 pro_l_work(OUR WORK 최신글)

---
+++ theme/propaganda/skin/latest/pro_l_work/latest.skin.php
@@ -0,0 +1,61 @@
+<?php
+if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
+// add_stylesheet('css 구문', 출력순서); 숫자가 작을 수록 먼저 출력됨
+add_stylesheet('<link rel="stylesheet" href="'.$latest_skin_url.'/style.css">', 0);
+include_once(G5_LIB_PATH.'/thumbnail.lib.php');
+$thumb_width = 280;
+$thumb_height = 400;
+
+/*
+프로젝트명 : 시즌10' CREATE GNUBOARD SKIN
+스킨튜닝개발자 : 흑횽TM 
+개발자사이트주소 : http://shoponex.com/?theme=skin
+라이선스 : 오픈소스 플러그인 라이선스 참고. 
+기타 라이선스 : 그누보드 사이트 (sir.kr)를 제외한 타 사이트에서 재배포 금지.
+*/
+
+?>
+
+
+<script type="text/javascript">
+			$(document).ready(function(){
+				$(".cover", this).stop().animate({top:'<?php echo $thumb_height?>px'},{queue:false,duration:160});		
+				//Full Caption Sliding (Hidden to Visible)
+				$('.boxgrid.captionfull').hover(function(){
+					$(".cover", this).stop().animate({top:'<?php echo $thumb_height-100?>px'},{queue:false,duration:160});					
+				}, function() {
+					$(".cover", this).stop().animate({top:'<?php echo $thumb_height?>px'},{queue:false,duration:160});
+				});
+				//Caption Sliding (Partially Hidden to Visible)
+				$('.boxgrid.caption').hover(function(){
+					$(".cover", this).stop().animate({top:'<?php echo $thumb_height-100?>px'},{queue:false,duration:160});
+				}, function() {
+					$(".cover", this).stop().animate({top:'<?php echo $thumb_height?>px'},{queue:false,duration:160});
+				});
+			});
+</script>
+
+
+<?php
+for ($i=0; $i<count($list); $i++) {	
+	$thumb = get_list_thumbnail($bo_table, $list[$i]['wr_id'], $thumb_width, $thumb_height);
+
+	if($thumb['src']) {
+		$thumb_url = $thumb['src'];
+	} else {
+		$thumb_url = $latest_skin_url."/img/no-image.gif";
+	}
+?>
+			<div class="boxgrid captionfull">
+				<img src="<?php echo $thumb['src']?>" alt="" />
+				<div class="cover boxcaption">
+					<h3><?php echo $list[$i]['subject']; ?></h3>
+					<p><?php echo cut_str(strip_tags($list[$i]['wr_content']),22); ?><br/>
+					<a href="<?php echo $list[$i]['href']?>">More</a></p>
+				</div>		
+			</div>
+	
+<?php	}	?>		
+
+<div style="clear:both"></div>
+
