83ec28b9a2f1da5c3ad11f0c07cb1eb28d90fe08
dbmlight <dbmlight@gmail.com>
2018-10-06 14:41:25 +0900

    list.php 수정 (for WORK) 전체 >> ALL

--- bbs/list.php
+++ bbs/list.php
@@ -11,7 +11,7 @@ if ($board['bo_use_category']) {
     $category_option .= '<li><a href="'.$category_href.'"';
     if ($sca=='')
         $category_option .= ' id="bo_cate_on"';
-    $category_option .= '>전체</a></li>';
+    $category_option .= '>ALL</a></li>';
 
     $categories = explode('|', $board['bo_category_list']); // 구분자가 , 로 되어 있음
     for ($i=0; $i<count($categories); $i++) {
